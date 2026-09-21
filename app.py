d48dd70095b931dcb72a7031c3a12b6fd9242ca0
Karina ☆ <karina.dev@proton.me>
2026-09-21 00:06:28 -0300

    Programa finalizado

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
-#Input. ♡
+# Input. ♡
 
 tipo_de_imovel = input("₊˚✩Digite o tipo de imóvel: ")
 consumo_mensal = float(input("₊˚✩Digite o consumo mensal em m³: "))
 
-#Processamento e saída. ♡
+# Processamento e saída. ♡
 
 if tipo_de_imovel == "comercial":
     print("Tarifa comercial aplicada — consulte o plano corporativo.")
-    import sys
-    sys.exit()
 
-elif tipo_de_imovel == "apartamento" and consumo_mensal <= 10:
+elif tipo_de_imovel == "apartamento" and consumo_mensal < 10:
     print("Consumo econômico — excelente consumo de água! ｡ﾟ+.ﾟ (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ﾟ.+ﾟ｡")
 
-if not consumo_mensal > 25:
-    print("Consumo moderado — dentro do padrão residencial. (◕‿◕)")
+
+elif consumo_mensal <= 25:
+     print("Consumo moderado — dentro do padrão residencial. (◕‿◕)")
 
 else:
     print("Consumo excessivo — adote medidas de economia e verifique vazamentos. (｡•́︿•̀｡)")
-
-
-
 
 
